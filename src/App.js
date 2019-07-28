a99811f69e39a5a25867fd4f734b6a39310c3edd
Nepcam <cameron.nepe@gmail.com>
2019-07-28 21:17:37 +1200

    fix ups

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
     this.setState({loading: true});
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
+      `https://api.github.com/search/users/${username}?client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
@@ -49,7 +49,7 @@ class App extends Component {
     this.setState({loading: true});
 
     const res = await axios.get(
-      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
+      `https://api.github.com/search/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
         process.env.REACT_APP_GITHUB_CLIENT_ID
       }&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
